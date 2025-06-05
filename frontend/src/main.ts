a50d1a12a3c1f023eb5407efb69681ad87abdc8d
Ruben <rvb1492@hotmail.com>
2025-06-05 18:00:43 +0200

    feat: commit inicial

---
+++ frontend/src/main.ts
@@ -0,0 +1,18 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+bootstrapApplication(AppComponent, {
+   providers: [
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+    ]
+})
+  .catch((err) => console.error(err));
